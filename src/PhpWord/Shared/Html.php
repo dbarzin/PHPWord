190416b0c4a20ef15feaa43ed886b71e65eda990
Didier Barzin <dbarzin@gmail.com>
2021-03-10 17:55:12 +0100

    Support for HTML <BR> tag

--- src/PhpWord/Shared/Html.php
+++ src/PhpWord/Shared/Html.php
@@ -62,7 +62,7 @@ class Html
         // Preprocess: remove all line ends, decode HTML entity,
         // fix ampersand and angle brackets and add body tag for HTML fragments
         $html = str_replace(array("\n", "\r"), '', $html);
-        $html = str_replace(array('&lt;', '&gt;', '&amp;', '&quot;'), array('_lt_', '_gt_', '_amp_', '_quot_'), $html);
+        $html = str_replace(array('&lt;', '&gt;', '&amp;', '&quot;','<br>'), array('_lt_', '_gt_', '_amp_', '_quot_','<br/>), $html);
         $html = html_entity_decode($html, ENT_QUOTES, 'UTF-8');
         $html = str_replace('&', '&amp;', $html);
         $html = str_replace(array('_lt_', '_gt_', '_amp_', '_quot_'), array('&lt;', '&gt;', '&amp;', '&quot;'), $html);
